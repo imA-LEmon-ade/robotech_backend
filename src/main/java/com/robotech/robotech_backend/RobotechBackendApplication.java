4559e9423f63117c94a648719582f95f879703a3
theWOng23 <junior2103rcd@gmail.com>
2025-12-18 18:14:27 -0500

    se volvio a hashear las contraseñas en la bd

--- src/main/java/com/robotech/robotech_backend/RobotechBackendApplication.java
+++ src/main/java/com/robotech/robotech_backend/RobotechBackendApplication.java
@@ -1,7 +1,11 @@
 package com.robotech.robotech_backend;
 
+import com.robotech.robotech_backend.repository.UsuarioRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class RobotechBackendApplication {
@@ -9,5 +13,6 @@ public class RobotechBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RobotechBackendApplication.class, args);
 	}
+
 
 }
--- src/main/java/com/robotech/robotech_backend/security/SecurityConfig.java
+++ src/main/java/com/robotech/robotech_backend/security/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
+
         return new BCryptPasswordEncoder();
     }
 
